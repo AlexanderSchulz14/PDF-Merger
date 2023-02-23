c9df9e9767cfb77525fbd57e4e61e8159423d69d
Alexander Schulz <120401861+AlexanderSchulz14@users.noreply.github.com>
2023-02-23 10:45:56 +0100

    Added sort to pdf list

--- Python_PDF_Merger.py
+++ Python_PDF_Merger.py
@@ -7,15 +7,16 @@ Created on Wed Jan 11 10:13:20 2023
 import PyPDF2
 import sys
 import os
+import re
 
 # WD
-# choose directory where PDFs to be merged are
+# choose directory where PDFs to be merged are stored
 # NOTE: ordering of files matters for merge!
 path = r"path/.../" # r for raw string
-path = path.replace("\\", "/")
+# path = path.replace("\\", "/")
 os.chdir(path)
 
-# Get Files
+# Get .pdf Files
 pdf_ls = []
 
 for file in os.listdir(path):
@@ -23,7 +24,8 @@ for file in os.listdir(path):
         print(file)
         pdf_ls.append(file)
         
- 
+# Sort files by extracted integers in strings from Filename
+pdf_ls = sorted(pdf_ls, key=lambda f: int(re.search(r"\d+", f).group()))
 # Merge       
 merger = PyPDF2.PdfMerger()
 
